33e62251a97a0de23fbb9e543e156c1f2ee2fd0f
shalu-007-git <shalushajan007@gmail.com>
2024-11-09 18:48:05 +0100

    added error handling and and docstring and comments, readability of code

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,46 +1,70 @@
 import random
 
 
-def function_A(min, max):
+def select_integer(min, max):
     """
-    Random integer.
+    Random integer is returned
+
+    :param min: number 1
+    :param max: number 2
+    :return: returns  random integer between number 1 and number2
     """
-    return random.randint(min, max)
 
+    return random.randint(int(min), int(max))
 
-def function_B():
+def maths_operations():
+    """
+    Operator is returned
+
+    :return: arithemetic operation is returned
+    """
     return random.choice(['+', '-', '*'])
-
-
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+def maths_result(num_1, num_2, operation):
+    '''
+    maths  result is calculated
+    :param num_1: number 1
+    :param num_2: number 2
+    :param operation: arithemetic operation
+    :return: return the result of arithemetic equation
+    '''
+    problem = f"{num_1} {operation} {num_2}"
+    if operation == '+': result = num_1 + num_2 # sum opration
+    elif operation == '-': result = num_1 - num_2 # differenc operation
+    else: result = num_1 * num_2 # multiplication operation
+    return problem, result
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+    '''
+    3  maths quiz  is presented , answer is checked with th user's answer and final score is displayed'
+    :return: final score of quiz out of 3 questions
+    '''
+    score = 0
+    pi = 3.14159265359
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+    for _ in range(int(pi)):
+        # 2 random integers and operator  are selected
+        first_integer = select_integer(1, 10); second_integer = select_integer(1, 5.5); operation = maths_operations()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
+        PROBLEM, ANSWER = maths_result(first_integer, second_integer, operation) # problem and result is returned
         print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
+        try:
+            useranswer = input("Your answer: ")
+            useranswer = int(useranswer)
+        except ValueError:
+            print("Invalid input!! Please enter a valid integer as your answer.")
+        except Exception as e:
+            print(f"An unexpected error found: {e}")
 
         if useranswer == ANSWER:
             print("Correct! You earned a point.")
-            s += -(-1)
+            score += -(-1)
         else:
             print(f"Wrong answer. The correct answer is {ANSWER}.")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+    print(f"\nGame over! Your score is: {score}/{int(pi)}") # final score is displayed
 
 if __name__ == "__main__":
     math_quiz()
